52363c55df9447e23508bb437ae5926ec6dc48b3
thecousinvinny <thecousinvinny@gmail.com>
2026-06-13 22:14:57 -0700

    Revert invalid eslint config option

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  eslint: { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [
       { hostname: 'images.pokemontcg.io' },
